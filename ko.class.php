efd196fdba278944b2cf02e6ff9737f7986e57d9
zhang chu <zhtrue@gmail.com>
2013-04-13 21:37:52 +0800

    将配置设置为常用配置

--- ko.class.php
+++ ko.class.php
@@ -1,222 +1,5 @@
 <?php
 /**
- * KO框架简介
- *
- * <b>KO在那里？</b>
- * <ul>
- * <li>svn://192.168.0.141/dev/code/php/kx/base/doc/ko框架</li>
- * <li>svn://192.168.0.141/dev/code/php/kx/base/www/include/ko</li>
- * </ul>
- *
- * <b>为什么使用KO？</b>
- * <ul>
- *   <li>
- *     数据层解决方案
- *     <ul>
- *       <li>
- *         IDMan
- *         <ul>
- *           <li>对 IDMan 进行了封装，一般不用再直接调用这个中间层的接口</li>
- *         </ul>
- *       </li>
- *       <li>
- *         DBMan
- *         <ul>
- *           <li>数据查询返回结果保存在数组而不是对象中，更方便使用</li>
- *           <li>限制了 SQL 调用的方式，不能直接拼写 SQL，禁止调用 REPLACE，完善的 INSERT 处理</li>
- *           <li>对数据库表唯一健进行了 Cache 化封装，支持进程内 Cache 和 memcache，降低了数据库不同步导致问题的概率</li>
- *           <li>改善了由于进程内缓存占用空间过大导致崩溃的情况</li>
- *           <li>支持使用字符串字段作为分表字段的数据表</li>
- *         </ul>
- *       </li>
- *       <li>
- *         UObjectMan
- *         <ul>
- *           <li>数据查询返回结果保存在数组而不是对象中，更方便使用</li>
- *           <li>与 DBMan 的 Cache 统一化</li>
- *         </ul>
- *       </li>
- *       <li>
- *         MCache
- *         <ul>
- *           <li>更好的 MCache 支持，能将某个 key 的内容设置为空串</li>
- *           <li>支持一种模块 MCache，使用模块 MCache 时，只要保证 key 值在模块内部唯一就可以了</li>
- *         </ul>
- *       </li>
- *       <li>
- *         直接连接数据库
- *         <ul>
- *           <li>更简单的使用方式</li>
- *         </ul>
- *       </li>
- *     </ul>
- *   </li>
- *   <li>
- *     逻辑层解决方案 - 模式库
- *     <ul>
- *       <li>
- *         Item
- *         <ul>
- *           <li>
- *             用一个数据表来观察另外一个数据表的变化情况，并记录
- *             一些数据表需要根据不同字段进行分表时，只需要进行简单的配置就可以完成
- *           </li>
- *         </ul>
- *       </li>
- *       <li>
- *         Limit
- *         <ul>
- *           <li>
- *             对某个对象的行为次数限制
- *           </li>
- *         </ul>
- *       </li>
- *       <li>
- *         Counter
- *         <ul>
- *           <li>
- *             可以支持增长非常快的实时计数器
- *           </li>
- *         </ul>
- *       </li>
- *       <li>
- *         User
- *         <ul>
- *           <li>
- *             通用的用户身份验证系统，用来确保用户登录过程和密码安全
- *           </li>
- *         </ul>
- *       </li>
- *       <li>
- *         OCMT
- *         <ul>
- *           <li>
- *             一种通用的评论系统
- *           </li>
- *         </ul>
- *       </li>
- *       <li>
- *         XList
- *         <ul>
- *           <li>
- *             快速生成一个数据库表的管理维护界面
- *           </li>
- *         </ul>
- *       </li>
- *       <li>
- *         更多的通用模式可添加 ......
- *       </li>
- *     </ul>
- *   </li>
- *   <li>
- *     表现层解决方案 - 视图驱动
- *     <ul>
- *       <li>模版与程序更彻底的分离</li>
- *     </ul>
- *   </li>
- *   <li>
- *     其他
- *     <ul>
- *       <li>与具体应用无关</li>
- *       <li>更简洁的文件命名方式</li>
- *       <li>UTF-8 和 GB18030 更完善的支持</li>
- *     </ul>
- *   </li>
- * </ul>
- *
- * <b>KO框架是什么？</b>
- * <ul>
- * <li>KO框架帮助应用简化和规范开发的过程，但是KO框架是一个与任何具体应用无关的框架</li>
- * <li>KO框架遵循三层结构的实现方式（数据层，逻辑层，表现层）</li>
- * </ul>
- *
- * <b>KO框架能做什么？</b>
- * <ul>
- * <li>KO框架能帮助php工程师简化每个环节的开发任务，让工程师的精力集中在处理数据库设计，业务相关和页面相关的逻辑上</li>
- * <li>KO框架能帮助新人尽快地进入开发状态</li>
- * <li>KO框架对常用的一些应用模式进行了总结，并开发了模式库，使开发更容易，更快，更少出错。</li>
- * <li>KO框架引入了视图驱动的概念，将模版与程序进行更彻底的分离。</li>
- * </ul>
- *
- * <b>KO框架的组成部分</b>
- * <ul>
- *   <li>
- *     工具模块
- *     <ul>
- *       <li>
- *         tool
- *         <ul>
- *           <li>这部分是一些公共的模块，单例处理，命名规则等函数</li>
- *         </ul>
- *       </li>
- *       <li>
- *         html
- *         <ul>
- *           <li>对 html 文本进行处理</li>
- *         </ul>
- *       </li>
- *     </ul>
- *   </li>
- *   <li>
- *     数据层模块
- *     <ul>
- *       <li>
- *         data
- *         <ul>
- *           <li>这部分实现对中间层函数的封装，完成返回数据的标准化</li>
- *           <li>中间层 DBAgent 的 SQL 简单封装</li>
- *           <li>中间层 UObject 的 MCache 和 LCache 缓存</li>
- *         </ul>
- *       </li>
- *       <li>
- *         dao
- *         <ul>
- *           <li>对 SQL 的进一步封装</li>
- *           <li>对分表字段/唯一字段/IDMan和DBMan关系的封装</li>
- *           <li>对 UObject 的进一步封装</li>
- *           <li>对直接连接数据库操作进行封装</li>
- *           <li>对数据表以外的其他类型数据操作进行封装</li>
- *         </ul>
- *       </li>
- *     </ul>
- *   </li>
- *   <li>
- *     逻辑层模块
- *     <ul>
- *       <li>
- *         busi
- *         <ul>
- *           <li>对逻辑层各层的调用限制进行封装</li>
- *         </ul>
- *       </li>
- *       <li>
- *         mode
- *         <ul>
- *           <li>对常用的一些应用逻辑进行封装，可能是逻辑层的，也可能是表现层的</li>
- *         </ul>
- *       </li>
- *     </ul>
- *   </li>
- *   <li>
- *     表现层模块
- *     <ul>
- *       <li>
- *         app
- *         <ul>
- *           <li>app基类定义了app的各处理方法及执行顺序</li>
- *           <li>封装了http协议的基本逻辑处理</li>
- *         </ul>
- *       </li>
- *       <li>
- *         view
- *         <ul>
- *           <li>支持视图驱动编程</li>
- *         </ul>
- *       </li>
- *     </ul>
- *   </li>
- * </ul>
- *
  * @package ko
  * @author zhangchu
  */
@@ -315,7 +98,7 @@ if(!defined('KO_PROXY'))
  */
 if(!defined('KO_DB_ENGINE'))
 {
-	define('KO_DB_ENGINE', 'kproxy');
+	define('KO_DB_ENGINE', 'mysql');
 }
 /**
  * 配置数据库主机端口，直连数据库的时候使用
@@ -394,7 +177,7 @@ if(!defined('KO_MONGO_SAFE'))
  */
 if(!defined('KO_ENC'))
 {
-	define('KO_ENC', 'IgBinary');
+	define('KO_ENC', 'Serialize');
 }
 
 /**
@@ -410,7 +193,7 @@ if(!defined('KO_IMAGE'))
  */
 if(!defined('KO_MC_ENGINE'))
 {
-	define('KO_MC_ENGINE', 'kproxy');
+	define('KO_MC_ENGINE', 'memcache');
 }
 /**
  * 配置使用的 memcache 服务器，KO_MC_ENGINE 为 memcache 有效
@@ -431,7 +214,7 @@ if(!defined('KO_LC_ENGINE'))
  */
 if(!defined('KO_REDIS_ENGINE'))
 {
-	define('KO_REDIS_ENGINE', 'kproxy');
+	define('KO_REDIS_ENGINE', 'redis');
 }
 /**
  * 配置使用的 redis 服务器，KO_REDIS_ENGINE 为 redis 有效
