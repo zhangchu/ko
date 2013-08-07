7c97942b63faeaafc3c053e660a6c3345a76de32
zhang chu <zhtrue@gmail.com>
2013-08-07 13:18:12 +0800

    增加拼音函数库

---
+++ tool/Pinyin.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Pinyin
+ *
+ * @package ko
+ * @subpackage tool
+ * @author zhangchu
+ */
+
+/**
+ * 拼音相关的函数接口，所有输入输出按照 UTF-8 处理
+ */
+interface IKo_Tool_Pinyin
+{
+	/**
+	 * 将音调与拼音分开，并将带音调的符号转换为 ascii 字符
+	 *
+	 * @return array
+	 */
+	public static function ASplitTone($sPyWithTone);
+	/**
+	 * 将拼音的声母和韵母分开
+	 *
+	 * @return array|boolean 返回 false 表示拼音错误
+	 */
+	public static function VSplitOnsetRime($sPy);
+}
+
+/**
+ * 拼音相关的函数实现，所有输入输出按照 UTF-8 处理
+ */
+class Ko_Tool_Pinyin implements IKo_Tool_Pinyin
+{
+	private static $s_aSyllableOnset = array(				//声母
+		'b', 'p', 'm', 'f', 'd', 't', 'n', 'l',
+		'g', 'k', 'h', 'j', 'q', 'x',
+		'zh', 'ch', 'sh', 'r', 'z', 'c', 's',
+		'y', 'w');
+	private static $s_aSyllableRime = array(				//韵母
+		'a', 'o', 'e', 'i', 'u', 'v',
+		'ai', 'ei', 'ao', 'ou', 'ia', 'ie', 'iao', 'iu', 'ua', 'uo', 'uai', 'ui', 'ue', 've', 'er',
+		'an', 'en', 'in', 'un',
+		'ang', 'eng', 'ing', 'ong', 'ng',
+		'ian', 'uan', 'iang', 'uang', 'iong',
+	);
+	private static $s_aVowelTone = array(
+		'a' => array('a', 0), 'ā' => array('a', 1), 'á' => array('a', 2), 'ǎ' => array('a', 3), 'à' => array('a', 4),
+		'o' => array('o', 0), 'ō' => array('o', 1), 'ó' => array('o', 2), 'ǒ' => array('o', 3), 'ò' => array('o', 4),
+		'e' => array('e', 0), 'ē' => array('e', 1), 'é' => array('e', 2), 'ě' => array('e', 3), 'è' => array('e', 4),
+		'i' => array('i', 0), 'ī' => array('i', 1), 'í' => array('i', 2), 'ǐ' => array('i', 3), 'ì' => array('i', 4),
+		'u' => array('u', 0), 'ū' => array('u', 1), 'ú' => array('u', 2), 'ǔ' => array('u', 3), 'ù' => array('u', 4),
+		'ü' => array('v', 0), 'ǖ' => array('v', 1), 'ǘ' => array('v', 2), 'ǚ' => array('v', 3), 'ǜ' => array('v', 4),
+	);
+	
+	/**
+	 * @return array
+	 */
+	public static function ASplitTone($sPyWithTone)
+	{
+		$len = mb_strlen($sPyWithTone, 'UTF-8');
+		$lasttone = 0;
+		$arr = array();
+		for ($i=0; $i<$len; ++$i)
+		{
+			$char = mb_substr($sPyWithTone, $i, 1, 'UTF-8');
+			$ret = self::_AGetASCIIVowelTone($char);
+			if (false === $ret)
+			{
+				$arr[] = $char;
+			}
+			else
+			{
+				$arr[] = $ret[0];
+				if ($ret[1])
+				{
+					$lasttone = $ret[1];
+				}
+			}
+		}
+		return array(implode('', $arr), $lasttone);
+	}
+	
+	/**
+	 * @return array|boolean
+	 */
+	public static function VSplitOnsetRime($sPy)
+	{
+		$onsetlen = 2;
+		$onset = substr($sPy, 0, $onsetlen);
+		if (!in_array($onset, self::$s_aSyllableOnset))
+		{
+			$onsetlen = 1;
+			$onset = substr($sPy, 0, $onsetlen);
+			if (!in_array($onset, self::$s_aSyllableOnset))
+			{
+				$onsetlen = 0;
+				$onset = '';
+			}
+		}
+		$rime = $onsetlen ? substr($sPy, $onsetlen) : $sPy;
+		if (in_array($rime, self::$s_aSyllableRime))
+		{	//有韵母
+			return array($onset, $rime);
+		}
+		if (in_array($sPy, self::$s_aSyllableRime))
+		{	//没声母，有韵母
+			return array('', $sPy);
+		}
+		if (0 === strlen($rime) && 0 !== strlen($onset))
+		{	//有声母，没韵母
+			return array($onset, '');
+		}
+		return false;
+	}
+	
+	private static function _AGetASCIIVowelTone($sVowel)
+	{
+		return isset(self::$s_aVowelTone[$sVowel]) ? self::$s_aVowelTone[$sVowel] : false;
+	}
+}
