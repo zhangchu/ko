2b10659ae3e6c8ec4ed80cb0d456c999769e594c
zhang chu <zhtrue@gmail.com>
2014-06-04 18:43:05 +0800

    增加Lock工具函数

---
+++ tool/Lock.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Lock
+ *
+ * @package ko
+ * @subpackage tool
+ * @author zhangchu
+ */
+
+interface IKo_Tool_Lock
+{
+	/**
+	 * 获取写锁，独占锁，不阻塞
+	 *
+	 * @return boolean
+	 */
+	public static function BGetExLock($sName);
+}
+
+class Ko_Tool_Lock implements IKo_Tool_Lock
+{
+	private static $s_aLockHandle = array();
+
+	/**
+	 * @return boolean
+	 */
+	public static function BGetExLock($sName)
+	{
+		$lockfp = fopen($sName.'.lock', 'w');
+		if (!$lockfp)
+		{
+			return false;
+		}
+		if (!flock($lockfp, LOCK_EX | LOCK_NB))
+		{
+			return false;
+		}
+		self::$s_aLockHandle[$sName] = $lockfp;
+		return true;
+	}
+}
