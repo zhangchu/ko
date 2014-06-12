1a5c2c5e8eb4b17643cac8b70d6ce941e0a0061f
zhang chu <zhtrue@gmail.com>
2014-06-12 10:34:35 +0800

    view/Base修改

--- view/Base.php
+++ view/Base.php
@@ -18,7 +18,12 @@ class Ko_View_Base
 	 */
 	public function __get($sName)
 	{
-		if(substr($sName, -3) === 'Api')
+		if ($sName == '_smarty')
+		{
+			$this->_smarty = Ko_Tool_Singleton::OInstance('Ko_View_Smarty');
+			return $this->_smarty;
+		}
+		else if(substr($sName, -3) === 'Api')
 		{
 			$this->$sName = Ko_Tool_Object::OCreateFromRoot($this, $sName);
 			return $this->$sName;
