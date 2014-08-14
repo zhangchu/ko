63a1004e7d42c61f393f931de914b01149be282e
zhang chu <zhtrue@gmail.com>
2014-08-14 13:38:45 +0800

    增加Ko_Tool_UA

---
+++ tool/UA.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * UA
+ *
+ * @package ko\tool
+ * @author zhangchu
+ */
+
+class Ko_Tool_UA
+{
+	private static $s_aSpiderKeys = array('spider', 'bot', 'slurp');
+	
+	/**
+	 * ÅÐ¶ÏUser-AgentÊÇ·ñÊÇÅÀ³æ
+	 *
+	 * @return boolean
+	 */
+	public static function BIsSpider($sUa)
+	{
+		$ua = strtolower($sUa);
+		foreach (self::$s_aSpiderKeys as $key)
+		{
+			if (false !== strpos($ua, $key))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+}
