9f8001ec73e1a9f5ba58cd4ad147ad9dfb29ec05
zhang chu <zhtrue@gmail.com>
2014-10-22 16:45:53 +0800

    判断ua是否是移动设备，手机或pad

--- tool/UA.php
+++ tool/UA.php
@@ -9,7 +9,9 @@
 class Ko_Tool_UA
 {
 	private static $s_aSpiderKeys = array('spider', 'bot', 'slurp');
-	
+	private static $s_aPhone = array('phone', 'android', 'symbian', 'smartphone', 'midp', 'wap', 'ipod');
+	private static $s_aPad = array('ipad', 'xoom');
+
 	/**
 	 * 判断User-Agent是否是爬虫
 	 *
@@ -38,6 +40,52 @@ class Ko_Tool_UA
 		if ('' === $sUa)
 		{
 			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * 判断User-Agent是否为移动设备，包括手机和pad
+	 *
+	 * @return boolean
+	 */
+	public static function BIsMobile($sUa)
+	{
+		return self::BIsPhone($sUa) || self::BIsPad($sUa);
+	}
+	
+	/**
+	 * 判断User-Agent是否是手机
+	 *
+	 * @return boolean
+	 */
+	public static function BIsPhone($sUa)
+	{
+		$ua = strtolower($sUa);
+		foreach (self::$s_aPhone as $key)
+		{
+			if (false !== strpos($ua, $key))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * 判断User-Agent是否是pad
+	 *
+	 * @return boolean
+	 */
+	public static function BIsPad($sUa)
+	{
+		$ua = strtolower($sUa);
+		foreach (self::$s_aPad as $key)
+		{
+			if (false !== strpos($ua, $key))
+			{
+				return true;
+			}
 		}
 		return false;
 	}
