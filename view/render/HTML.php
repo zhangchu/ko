8de0cf82544ed640c386da3f87ca9aa6f1712ec1
zhang chu <zhtrue@gmail.com>
2015-01-20 15:34:28 +0800

    基于内容中心来渲染html

---
+++ view/render/HTML.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * HTML
+ *
+ * @package ko\view\render
+ * @author zhangchu
+ */
+
+class Ko_View_Render_HTML extends Ko_View_Render_Base
+{
+	private $_oContentApi;
+
+	/**
+	 * oSetData($aid, $id);
+	 * oSetData($aid, $ids);
+	 * oSetData($aid, array('ids' => [, 'maxlength' => ]);
+	 */
+	public function __construct(Ko_Mode_Content $api)
+	{
+		$this->_oContentApi = $api;
+	}
+	
+	protected function _sRender()
+	{
+		$singleAid = (1 === count($this->_aData));
+		$singleId = false;
+		foreach ($this->_aData as $aid => &$data)
+		{
+			if (!is_array($data))
+			{
+				$data = array($data);
+				if ($singleAid)
+				{
+					$singleId = true;
+				}
+			}
+		}
+		unset($data);
+		
+		$htmllist = $this->_oContentApi->aGetHtmlEx($this->_aData);
+		
+		if ($singleAid)
+		{
+			foreach ($htmllist as $aid => $htmls)
+			{
+				if ($singleId)
+				{
+					foreach ($htmls as $id => $html)
+					{
+						return $html;
+					}
+				}
+				return $htmls;
+			}
+		}
+		return $htmllist;
+	}
+}
