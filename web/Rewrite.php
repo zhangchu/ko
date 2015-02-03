fdef9582957439fd2b070b9b407b5a58f62e2131
zhang chu <zhtrue@gmail.com>
2015-02-03 14:36:40 +0800

    rewrite后修改环境变量

--- web/Rewrite.php
+++ web/Rewrite.php
@@ -92,12 +92,33 @@ class Ko_Web_Rewrite
     
     private static function _VResetEnv($location)
     {
-        list(, $qs) = explode('?', $location, 2);
+        list($sn, $qs) = explode('?', $location, 2);
         parse_str($qs, $arr);
         $GLOBALS['_GET'] = $_GET = $arr;
         $GLOBALS['_REQUEST'] = $_REQUEST = $_REQUEST + $arr;
-        $GLOBALS['_SERVER']['QUERY_STRING'] = $_SERVER['QUERY_STRING'] = $qs;
+        $GLOBALS['_SERVER']['QUERY_STRING'] = $_SERVER['QUERY_STRING'] =
         $GLOBALS['_ENV']['QUERY_STRING'] = $_ENV['QUERY_STRING'] = $qs;
+        
+        if (false !== ($pos = strpos($sn, '.php/')))
+        {
+            $pi = substr($sn, $pos + 4);
+            $sn = substr($sn, 0, $pos + 4);
+        }
+        else
+        {
+            $pi = '';
+        }
+        $GLOBALS['_SERVER']['PHP_SELF'] = $_SERVER['PHP_SELF'] =
+        $GLOBALS['_SERVER']['SCRIPT_NAME'] = $_SERVER['SCRIPT_NAME'] =
+        $GLOBALS['_ENV']['PHP_SELF'] = $_ENV['PHP_SELF'] =
+        $GLOBALS['_ENV']['SCRIPT_NAME'] = $_ENV['SCRIPT_NAME'] = $sn;
+        
+        $GLOBALS['_SERVER']['SCRIPT_FILENAME'] = $_SERVER['SCRIPT_FILENAME'] =
+        $GLOBALS['_ENV']['SCRIPT_FILENAME'] = $_ENV['SCRIPT_FILENAME'] =
+            Ko_Web_Request::SDocumentRoot().$sn;
+        
+        $GLOBALS['_SERVER']['PATH_INFO'] = $_SERVER['PATH_INFO'] =
+        $GLOBALS['_ENV']['PATH_INFO'] = $_ENV['PATH_INFO'] = $pi;
     }
     
     private static function _SMatchPath($paths, $rules, &$aKeys)
