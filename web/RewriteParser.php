a83d8f64ceff2fe8f9bc18f4567556853e695433
zhang chu <zhtrue@gmail.com>
2015-04-02 15:05:34 +0800

    优化rewriteparser

--- web/RewriteParser.php
+++ web/RewriteParser.php
@@ -17,63 +17,62 @@
  */
 class Ko_Web_RewriteParser
 {
-    public static function AProcess($sText)
-    {
-        $rules = array();
-        foreach (self::_aText2Array($sText) as $pattern => $rewrited)
-        {
-            Ko_Tool_Array::VOffsetSet($rules, $pattern, $rewrited, '/');
-        }
-        return $rules;
-    }
+	public static function AProcess($sText)
+	{
+		$rules = array();
+		foreach (self::_aText2Array($sText) as $pattern => $rewrited)
+		{
+			Ko_Tool_Array::VOffsetSet($rules, $pattern, $rewrited, '/');
+		}
+		return $rules;
+	}
 
-    private static function _AText2Array($sText)
-    {
-        $arr = array();
-        foreach (explode("\n", $sText) as $line)
-        {
-            list($line, $comment) = explode('#', $line, 2);
-            $line = trim($line);
-            if (0 === strlen($line))
-            {
-                continue;
-            }
-            list($pattern, $rewrited) = explode (' ', $line, 2);
-            if ('^' === substr($pattern, 0, 1))
-            {
-                $pattern = substr($pattern, 1);
-            }
-            if ('/' === substr($pattern, 0, 1))
-            {
-                $pattern = substr($pattern, 1);
-            }
-            if ('$' === substr($pattern, -1))
-            {
-                $pattern = substr($pattern, 0, -1);
-                $tag = '$';
-            }
-            else
-            {
-                $tag = '*';
-            }
-            $pattern .= '/'.$tag;
-            $arr[$pattern] = self::_SNormalizeRule($rewrited);
-        }
-        return $arr;
-    }
+	private static function _AText2Array($sText)
+	{
+		$arr = array();
+		foreach (explode("\n", $sText) as $line)
+		{
+			list($line, $comment) = explode('#', $line, 2);
+			$line = trim($line);
+			if (0 === strlen($line))
+			{
+				continue;
+			}
+			list($pattern, $rewrited) = explode (' ', $line, 2);
+			$pattern = ltrim($pattern, '^');
+			$pattern = ltrim($pattern, '/');
+			if ('$' === substr($pattern, -1))
+			{
+				$pattern = substr($pattern, 0, -1);
+				$tag = '$';
+			}
+			else if ('*' === substr($pattern, -1))
+			{
+				$pattern = substr($pattern, 0, -1);
+				$tag = '*';
+			}
+			else
+			{
+				$tag = '*';
+			}
+			$pattern .= '/'.$tag;
+			$arr[$pattern] = self::_SNormalizeRule($rewrited);
+		}
+		return $arr;
+	}
 
-    private static function _SNormalizeRule($sUri)
-    {
-        list($location, $httpCode) = explode(' ', trim($sUri), 2);
-        if ('/' !== substr($location, 0, 1))
-        {
-            $location = '/'.$location;
-        }
-        $httpCode = trim($httpCode);
-        if ('301' !== $httpCode && '302' !== $httpCode)
-        {
-            return $location;
-        }
-        return $location.' '.$httpCode;
-    }
+	private static function _SNormalizeRule($sUri)
+	{
+		list($location, $httpCode) = explode(' ', trim($sUri), 2);
+		if ('/' !== substr($location, 0, 1))
+		{
+			$location = '/'.$location;
+		}
+		$httpCode = trim($httpCode);
+		if ('301' !== $httpCode && '302' !== $httpCode)
+		{
+			return $location;
+		}
+		return $location.' '.$httpCode;
+	}
 }
