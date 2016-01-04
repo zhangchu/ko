8082439c3611add4f02537fa4929bc890fa70a33
zhangchu <zhtrue@gmail.com>
2016-01-04 17:05:31 +0800

    debug

--- tool/Option.php
+++ tool/Option.php
@@ -42,28 +42,41 @@ class Ko_Tool_Option
 	public static function SEscapeWhere($aArgs)
 	{
 		$iArgNum = count($aArgs);
-		assert($iArgNum && false === strpos($aArgs[0], '\'') && false === strpos($aArgs[0], '"'));
-
-		$where = $aArgs[0];
-		$pos = 0;
-		for ($i=1; $i<$iArgNum; ++$i)
+		if (1 === $iArgNum && is_array($aArgs[0]))
 		{
-			$pos = strpos($where, '?', $pos);
-			if (false === $pos)
+			$where = array();
+			foreach ($aArgs[0] as $k => $v)
 			{
-				break;
+				$field = ('`' === $k[0]) ? $k : '`'.$k.'`';
+				$where[] = '('.$field.' = "'.Ko_Data_Mysql::SEscape($v).'")';
 			}
-			if (is_array($aArgs[$i]))
+			$where = implode(' AND ', $where);
+		}
+		else
+		{
+			assert($iArgNum && false === strpos($aArgs[0], '\'') && false === strpos($aArgs[0], '"'));
+
+			$where = $aArgs[0];
+			$pos = 0;
+			for ($i=1; $i<$iArgNum; ++$i)
 			{
-				$escapeArg = array_map(array('Ko_Data_Mysql', 'SEscape'), $aArgs[$i]);
-				$sReplace = '"'.implode('", "', $escapeArg).'"';
+				$pos = strpos($where, '?', $pos);
+				if (false === $pos)
+				{
+					break;
+				}
+				if (is_array($aArgs[$i]))
+				{
+					$escapeArg = array_map(array('Ko_Data_Mysql', 'SEscape'), $aArgs[$i]);
+					$sReplace = '"'.implode('", "', $escapeArg).'"';
+				}
+				else
+				{
+					$sReplace = '"'.Ko_Data_Mysql::SEscape($aArgs[$i]).'"';
+				}
+				$where = substr($where, 0, $pos).$sReplace.substr($where, $pos + 1);
+				$pos += strlen($sReplace);
 			}
-			else
-			{
-				$sReplace = '"'.Ko_Data_Mysql::SEscape($aArgs[$i]).'"';
-			}
-			$where = substr($where, 0, $pos).$sReplace.substr($where, $pos + 1);
-			$pos += strlen($sReplace);
 		}
 		return $where;
 	}
