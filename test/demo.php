b75649da87ad42566004b98a6497b78cfa1cde26
zhangchu <zhtrue@gmail.com>
2016-09-01 18:52:39 +0800

    test

---
+++ test/demo.php
@@ -0,0 +1,12 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+include_once('../ko.class.php');
+
+class Ko_Test_demo extends TestCase
+{
+	public function test()
+	{
+		$this->assertEquals(0, 0);
+	}
+}
